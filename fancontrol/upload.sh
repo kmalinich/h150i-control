a66f465c911329ed5490d67ac3b808d5b0e61b6a
kdm <kmalinich@users.noreply.github.com>
2021-04-20 12:01:35 -0400

    Update

--- fancontrol/upload.sh
+++ fancontrol/upload.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
-BOARD="arduino:avr:leonardo"
+BOARD="arduino:avr:uno"
 OUTPUT_DIR="${PWD}/build"
 
 PORT_DEF="$(serialport-list --format json | jq -r '.[] | select(.manufacturer != null) | select(.manufacturer | contains("Arduino")) | .path')"
 PORT="${1-$PORT_DEF}"
 
-# Install CAN-BUS Shield library if missing
-if ! arduino-cli lib list --format json | jq -r '.[].library.name' | grep -Eq 'CAN\-BUS_Shield'; then
-	if ! arduino-cli lib install 'CAN-BUS Shield'; then
-		echo "Failed to install missing library, cannot continue"
+# Install libraries if missing
+if ! arduino-cli lib list --format json | jq -r '.[].library.name' | grep -Eq 'FanController'; then
+	if ! arduino-cli lib install 'FanController'; then
+		echo "Failed to install missing library 'FanController', cannot continue"
 		exit 1
+	fi
+fi
+
+if ! arduino-cli lib list --format json | jq -r '.[].library.name' | grep -Eq 'PID_v2'; then
+	if ! arduino-cli lib install 'PID_v2'; then
+		echo "Failed to install missing library 'PID_v2', cannot continue"
+		exit 2
 	fi
 fi
 
